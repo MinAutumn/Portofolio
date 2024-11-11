341f2991f02d66637eb811cc0a1aa5bc2325bb5e
Muhammad Ridhwan Khalid <m.ridhwankhalid@gmail.com>
2024-11-11 14:22:06 +1100

    finished navbar
    navbar is done, only need to add link to pages

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -3,11 +3,20 @@ import {darkTheme, darktheme} from "./utils/Themes";
 import Navbar from "./component/Navbar";
 import { BrowserRouter, Router } from "react-router-dom";
 
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg};
+  width: 100%;
+  height: 90vh;
+  overflow-x: hidden;
+  position: relative;
+`;
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}> 
       <BrowserRouter>
         <Navbar />
+        <Body />
       </BrowserRouter>
     </ThemeProvider>
   );
--- portofolio/src/component/Navbar.jsx
+++ portofolio/src/component/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  { Link as LinkR } from 'react-router-dom'
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { Bio } from "../data/constants";
 import { MenuRounded } from "@mui/icons-material";
 
@@ -29,6 +29,8 @@ const NavbarContainer = styled.div`
 const NavLogo = styled(LinkR)`
     width: 80%;
     padding: 0 6px;
+    font-weight: 500;
+    font-size: 18px;
     text-decoration: none;
     color: inherit;
 `;
@@ -69,6 +71,16 @@ const ButtonContainer = styled.div`
         display: none;
     }
 `;
+
+const MenuButtonContainer = styled.div`
+    width: 80%;
+    height: 100%;
+    display: flex;
+    gap: 15px;
+    justify-content: start;
+    align-items: start;
+`;
+
 const GithubButton = styled.div`
     border: 1px solid ${({theme}) => theme.primary};
     color: ${({theme}) => theme.primary};
@@ -112,17 +124,50 @@ const MobileIcon = styled.div`
     display:  flex;
     align-items:  center;
     color:  ${({theme}) => theme.text_primary};
+    display: none;
+    @media  screen and (max-width: 768px){
+        display: block;
+    }
+
 `;
 
+const MobileMenu = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    gap:  32px;
+    padding: 0 6px;
+    list-style: none;
+    width: 100%;
+    padding: 12px 40px 24px 40px;
+    background:  ${({theme}) => theme.card_light + 99};
+    position: absolute;
+    top: 80px;
+    right: 0;
+
+    transition: all 0.6s ease-in-out;
+    transform: ${({isOpen}) => 
+        isOpen ? "translateY(0)" : "translateY(-100%)"};
+    border-radius: 0 0 20px 20px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    opacity: ${({isOpen}) => (isOpen ? "100%" : "0")};
+    z-index: ${({isOpen}) => (isOpen ? "1000" : "-1000")};
+`;
 
 const Navbar = () => {
+    const theme = useTheme();
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <Nav>
         <NavbarContainer>
             <NavLogo to="/">My Portofolio</NavLogo>
-            <MobileIcon>
+
+            <MobileIcon onClick={() => setIsOpen(!isOpen)}>
                 <MenuRounded style={{ color: "inherit"}} />
             </MobileIcon>
+
             <NavItems>
                 <NavLink href="#About">About</NavLink>
                 <NavLink href="#Skills">Skills</NavLink>
@@ -130,6 +175,30 @@ const Navbar = () => {
                 <NavLink href="#Projects">Projects</NavLink>
                 <NavLink href="#Education">Education</NavLink>
             </NavItems>
+
+            {isOpen && ( 
+                <MobileMenu isOpen={isOpen}>
+                    <NavLink onClick={() => setIsOpen(!isOpen)} href="#About">About</NavLink>
+                    <NavLink onClick={() => setIsOpen(!isOpen)} href="#Skills">Skills</NavLink>
+                    <NavLink onClick={() => setIsOpen(!isOpen)} href="#Experience">Experience</NavLink>
+                    <NavLink onClick={() => setIsOpen(!isOpen)} href="#Projects">Projects</NavLink>
+                    <NavLink onClick={() => setIsOpen(!isOpen)} href="#Education">Education</NavLink>
+                    <MenuButtonContainer>
+                        <LinkedinButton href={Bio.linkedin} target="_Blank" style={{
+                            background: theme.linkedin,
+                            color: theme.text_primary
+                        }}>
+                            LinkedIn Profile
+                        </LinkedinButton>
+                        <GithubButton href={Bio.github} target="_Blank" style={{
+                            background: theme.primary,
+                            color: theme.text_primary
+                        }}>
+                            Github Profile
+                        </GithubButton>
+                    </MenuButtonContainer>
+                </MobileMenu>
+            )}
 
             <ButtonContainer>
                 <LinkedinButton href={Bio.linkedin} target="_Blank">
